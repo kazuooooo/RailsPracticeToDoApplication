ecfe7aa7a50010bd7bc18302d4894a14f66264cc
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-08 21:10:28 +0900

    fix indent

--- ToDoApplication/app/models/user.rb
+++ ToDoApplication/app/models/user.rb
@@ -6,18 +6,17 @@ class User < ActiveRecord::Base
 
   #新規登録か通常ログインなのかを判定
   def self.from_omniauth(auth)
-  	where(auth.slice(:provider, :uid).to_hash).first_or_create do |user|
-  		user.email = auth.info.email
-  		user.password = Devise.friendly_token[0,20]
-  	end
+    where(auth.slice(:provider, :uid).to_hash).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 
   def self.new_with_session(params, session)
-  	super.tap do |user|
-  		if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  		user.email = data["email"] if user.email.blank?
-  		end
-  	end
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      user.email = data["email"] if user.email.blank?
+      end
+    end
   end
-
 end
