1945c1b03025cab7251f8ca775b4099ae97beacf
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-08 19:39:10 +0900

    fix on showview delete

--- ToDoApplication/app/controllers/users/omniauth_callbacks_controller.rb
+++ ToDoApplication/app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,8 +4,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		@user = User.from_omniauth(request.env["omniauth.auth"])
 
 		if @user.persisted?
-
-			sign_in_and_redirect @user, :event => :authentication
+			#TODO:TaskModelを作ったら修正
+			sign_in_and_redirect @user
+			#sign_in_and_redirect @user, :event => :authentication
 			set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
 		else
 			session["devise.facebook_data"] = request.env["omniauth.auth"]
--- ToDoApplication/app/models/user.rb
+++ ToDoApplication/app/models/user.rb
@@ -7,7 +7,6 @@ class User < ActiveRecord::Base
   #新規登録か通常ログインなのかを判定
   def self.from_omniauth(auth)
   	where(auth.slice(:provider, :uid).to_hash).first_or_create do |user|
-  		binding.pry
   		user.email = auth.info.email
   		user.password = Devise.friendly_token[0,20]
   	end
