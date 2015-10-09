dbfb5057a10f3002f58d9a65650c8b829c11a747
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 15:00:57 +0900

    rename omniauth callback controller test class name

--- ToDoApplication/test/controllers/users/omniauth_callbacks_controller_test.rb
+++ ToDoApplication/test/controllers/users/omniauth_callbacks_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OmniauthCallbacksControllerTest < ActionController::TestCase
+class Users::OmniauthCallbacksControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
