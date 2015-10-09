fb6d5b3dc6702a440a3a4df3e2fe7be46534c0b8
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 10:57:22 +0900

    make scaffold

--- ToDoApplication/db/schema.rb
+++ ToDoApplication/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008045512) do
+ActiveRecord::Schema.define(version: 20151009020857) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "content",     limit: 65535
+    t.datetime "plan_date"
+    t.datetime "actual_date"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
