2abe08997514f09f8fa001dce02a72ba0ed37927
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 16:54:47 +0900

    redirect to taskindex when login rememberd

--- ToDoApplication/app/controllers/home_controller.rb
+++ ToDoApplication/app/controllers/home_controller.rb
@@ -1,9 +1,24 @@
 class HomeController < ApplicationController
    # ユーザがログインしていないと"show"にアクセスできない
-  before_action :authenticate_user!, only: :show
+  before_action :authenticate_user!, only: :tasks_path
+  before_action :redirect_to_task_index_if_login_rememberd
+
   def index
   end
 
   def show
   end
+
+  def redirect_to_task_index_if_login_rememberd
+    if user_signed_in? && root_page?
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def root_page?
+    return '/' === request.env["PATH_INFO"]
+  end
+
 end
