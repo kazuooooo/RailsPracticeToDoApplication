294c5b5348397f44c631fcdf486914fa2b760307
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 22:03:05 +0900

    make relation between user and task

--- ToDoApplication/db/schema.rb
+++ ToDoApplication/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009020857) do
+ActiveRecord::Schema.define(version: 20151009121845) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151009020857) do
     t.datetime "actual_date"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
 
   create_table "users", force: :cascade do |t|
