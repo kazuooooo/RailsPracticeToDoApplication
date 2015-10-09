08f9ac24987794e512c5ab038daa1dc3232eb12a
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 13:23:02 +0900

    not show actual date on new page

--- ToDoApplication/app/controllers/application_controller.rb
+++ ToDoApplication/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  helper_method :edit_page?
+
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_fir(resource)
    tasks_path
+  end
+
+  def edit_page?
+    return /\/tasks\/[0-9]\/edit/ === request.env["PATH_INFO"]
   end
 end
