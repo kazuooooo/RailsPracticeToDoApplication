c07e26c674fa5bb8d0c81de3b2ef0ede4f9a6fbe
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-09 10:57:22 +0900

    make scaffold

---
+++ ToDoApplication/app/views/tasks/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @task, :id, :title, :content, :plan_date, :actual_date, :created_at, :updated_at
--- ToDoApplication/db/schema.rb
+++ ToDoApplication/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008005612) do
+ActiveRecord::Schema.define(version: 20151009014358) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "content",     limit: 255
+    t.datetime "plan_date"
+    t.datetime "actual_date"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +35,8 @@ ActiveRecord::Schema.define(version: 20151008005612) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
