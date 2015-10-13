bfcce40d9cd9c0c56ef8b619e46f7fd1e76ca807
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-13 11:09:20 +0900

    make simple redirect to task in homecontroller

--- ToDoApplication/app/controllers/home_controller.rb
+++ ToDoApplication/app/controllers/home_controller.rb
@@ -1,16 +1,15 @@
 class HomeController < ApplicationController  
-  before_action :redirect_to_task_index_if_login_rememberd
-
-  def redirect_to_task_index_if_login_rememberd
-    if user_signed_in? && root_page?
-      redirect_to tasks_path
-    end
-  end
+  
+  before_filter :redirect_to_tasks, if: :user_signed_in?, only: :index
 
   private
 
   def root_page?
     return '/' === request.env["PATH_INFO"]
+  end
+
+  def redirect_to_tasks
+    redirect_to tasks_url
   end
 
 end
