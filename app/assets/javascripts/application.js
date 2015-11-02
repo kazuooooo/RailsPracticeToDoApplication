762a60e002de94024bf27c812716729210e7ba2a
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-11-02 15:55:56 +0900

    js css load order fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require_tree .
 //= require bootstrap-datepicker
 //= require jquery-tablesorter
+//= require_tree .
